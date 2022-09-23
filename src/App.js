0c641e65fa9607520e9f41a9f28554cac68ff42b
VirginieT <vtouzalin33@gmail.com>
2022-09-23 23:36:21 +0200

    Api space X core + info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState([]);
+  const [info, setInfo] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/capsules')
+      .then(response => response.json())
+      .then(data => setCount(data));
+  }, []);
+
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/info')
+      .then(response => response.json())
+      .then(data => setInfo(data));
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>SpaceX Capsules</h1>
+        {count.map((item) => (
+          <div key={item.capsule_serial}>
+            <h2>{item.capsule_serial}</h2>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h1>SpaceX Info</h1>
+          <div>
+            <h2>{info.name}</h2>
+          </div>
+      </div>
+    </>
   );
-}
+};   
 
 export default App;
